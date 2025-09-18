376ea8aeb6d0b0fbcb5ae63bc837b37371f1fab0
Kostya-Rozh <kosta7497@gmail.com>
2025-09-18 18:25:49 +0300

    added closed menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,15 @@
 document.getElementById('burger').onclick = function(){
     document.getElementsByClassName('menu-container')[0].style.display = 'block';
+
+    document.getElementsByClassName('menu-container')[0].onclick = function(){
+        document.getElementsByClassName('menu-container')[0].style.display = 'none';
+    };
+    document.getElementsByClassName('menu-item')[0].onclick = function(){
+        document.getElementsByClassName('menu-container')[0].style.display = 'none';
+    };
 }
 
 document.getElementById('close').onclick = function(){
     document.getElementsByClassName('menu-container')[0].style.display = 'none';
 }
+
